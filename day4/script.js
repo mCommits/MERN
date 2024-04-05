665b0060b0538b8a9e16fe1ce3edb17f85314741
mCommits <165825168+mCommits@users.noreply.github.com>
2024-04-05 10:53:13 +0530

    Day 4 - topic 1

---
+++ day4/script.js
@@ -0,0 +1,109 @@
+/* let a = new String("Heelo")
+let b = "Heelo"
+
+if (a == b) {
+  console.log(
+    "YEs"
+  )
+} else {
+  console.log("No")
+} */
+
+
+/* const obj = {
+  "name": "shad",
+} */
+
+/* console.log(obj);
+document.write(obj);
+const str = JSON.stringify(obj);
+console.log(str)
+
+document.write(str); */
+/* obj.age = 21;
+const str = JSON.stringify(obj);
+console.log(str) */
+
+
+
+
+//Array
+
+
+/* let arr = [];
+console.log(arr)
+
+arr.push(23);
+
+console.log(arr)
+
+
+arr = [2, 4, 5, 4, 4, 4];
+console.log(arr)
+
+arr.sort();
+arr[10] = 2;
+
+console.log(arr)
+console.log(typeof (arr)) */
+
+
+
+/* const ob = {
+  "name": "shadman",
+  "lname": "ahmed",
+} */
+
+/*
+
+const arr = [1, 24, 4, 4, 4];
+
+console.log(Array.isArray(arr)) */
+
+
+/* function checkIfObject(obj) {
+  return (Array.isArray(obj));
+}
+
+let res = checkIfObject([1, 3, 3, 3]);
+if (res == true) console.log("It is an array")
+else console.log("Not an array") */
+
+
+
+/* const jsonmaker = JSON.stringify(ob);
+console.log(jsonmaker)
+document.write(jsonmaker)
+let arr2 = [1, 3, 4, 5]
+const x = {
+  "name": "shadman",
+  age: 22,
+} */
+/* arr2.forEach(element => {
+  console.log(
+    element
+  )
+
+});
+*/
+/* x.forEach(element => {
+  console.log(element)
+}); */
+
+/* let arr2 = [1, 2, 2, 4, 5, 6, , 3]
+const x = {
+  "name": "shadma",
+  age: 12,
+}
+for (let i of arr2) {
+  console.log(i)
+}
+
+for (let i in x) {
+  console.log(i)
+} */
+
+
+
+
+
